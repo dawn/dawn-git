163df62a5e6772af78a808de4ba90db654a80023
IceDragon200 <mistdragon100@gmail.com>
2014-06-11 21:03:49 -0500

    BaseController now inherits from Sinatra::Base
    With a few hacks that is.

--- lib/dawn/git/controllers/base_controller.rb
+++ lib/dawn/git/controllers/base_controller.rb
@@ -3,28 +3,43 @@ require "sinatra/base"
 
 module Dawn
   module Git
-    class BaseController
-
-      include Sinatra::Helpers
-      include Sinatra::Templates
+    class BaseController < Sinatra::Base
 
       attr_reader :params
+      attr_reader :body_data
       attr_reader :request
       attr_reader :response
 
-      def call(request, response, name, params={})
+      def action(name, request, response, params={})
         @request = request
         @response = response
-        @params = Hash[params]
+        @params = params
+        @body_data = JSON.load(request.body)
+
+        content_type :json
+
         send name
       end
 
-      def json(data)
-        response.body = data.to_json
+      helpers do
+        def json(data)
+          response.body = data.to_json
+        end
       end
 
       def self.action(name)
-        ->(request, response, params={}) { new.call(request, response, name, params) }
+        lambda do |request, response, params={}|
+          # now this is a hack I'd rather avoid, but...
+          # The request and response bodies are passed from the original
+          # route block into the controller
+          # The call! method is avoided, since it will overwrite the
+          # response/request objects, so instead an action method was created
+          # to perform the same job.
+          # When control is returned to the route caller it will finalize
+          # the response and all should/will be well.
+          # Controller Middleware trickery!
+          new!.action(name, request, response, params)
+        end
       end
 
     end
